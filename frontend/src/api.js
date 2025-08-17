c58da492d82acd129a4ca6cb0c68ae90d01f6c4d
saibhargav2473 <saibhargav51020@gmail.com>
2025-08-17 18:33:23 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL:  'https://champo-backend.onrender.com',
+  baseURL:  'https://champo-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
